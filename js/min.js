ff39bf740efe4b01f30db510e6b08eaf0cea2593
sanjeevlodhi <133765697+sanjeevlodhi@users.noreply.github.com>
2024-09-26 16:12:53 +0800

    Add files via upload

--- js/min.js
+++ js/min.js
@@ -66,19 +66,3 @@ window.addEventListener("scroll", () => {
     header.classList.remove(toggleClass);
   }
 });
-
-
-// Form post valid
-// Form valid
-
-// const scriptURL = ''
-
-// const form = document.forms['contact-form']
-
-// fprm.addEventListener('submit', e => {
-//   e.preventDefault()
-//   fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-//     .then(response => alert("Thank you! your form is submitted successfully."))
-//     .then(() => { winsow.location.reload(); })
-//     .catch(error => console.error('Error!', error.message))
-// })
